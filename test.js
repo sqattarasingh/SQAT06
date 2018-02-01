010a36500a831f0d510c8bf2ddd87fa158a501eb
Izzat Bamieh <ibamieh@figure1.com>
2018-02-01 16:05:30 -0500

    Add test to go to duck duck go and read the title

---
+++ test.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const webdriverio = require('webdriverio');
+const options = { desiredCapabilities: { browserName: 'chrome' } };
+const client = webdriverio.remote(options);
+
+client
+    .init()
+    .url('https://duckduckgo.com/')
+    .setValue('#search_form_input_homepage', 'WebdriverIO')
+    .click('#search_button_homepage')
+    .getTitle().then(function(title) {
+        console.log('Title is: ' + title);
+    })
+    .end();
