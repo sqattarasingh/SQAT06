672fcaea397b5873086e74f3b662f7442dd073a1
Izzat Bamieh <ibamieh@figure1.com>
2018-03-02 13:19:38 -0500

    Add Figure1 test

--- features/support/steps.js
+++ features/support/steps.js
@@ -30,3 +30,41 @@ Then('I should see the search results', function(callback) {
     callback(error);
   })
 })
+
+Given('I go to figure1', function(callback) {
+  this.browser
+    .init()
+    .url('https://app.figure1.com/account/register').then(function() {
+      callback();
+    })
+})
+
+When('I sign up with valid credentials', function(callback) {
+  this.browser
+    .waitForVisible('.register-page__username-input')
+    .setValue('.register-page__username-input', 'ghjghjsdfgjdfgdfjh')
+    .setValue('.register-page__email-input', 'ghjghjsdfgjdfgdfjh@gmail.com')
+    .setValue('.register-page__password-input', 'ghjghjsdfgjdfgdfjh')
+    .setValue('.register-page__confirm-password-input', 'ghjghjsdfgjdfgdfjh')
+    .addValue('.register-page__specialties-list', 'Physician')
+    .addValue('.register-page__specialties-other-list', 'Anesthesiology')
+    .scroll('.register-page__terms-checkbox')
+    .click('.register-page__terms-checkbox')
+    .click('.register-page__submit-button')
+    .then(function(){
+      callback();
+    }).catch(function(error){
+    callback(error);
+  })
+})
+
+Then('I should see the search results', function(callback) {
+  this.browser
+    .getTitle().then(function(result){
+        result.should.equal("WebdriverIO at DuckDuckGo");
+        callback();
+  }).catch(function(error){
+    callback(error);
+  })
+})
+
